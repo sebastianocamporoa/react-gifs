2cd337143e0cabc53d9506438331367252afd512
Sebastian Ocampo Roa <sebastian45roa@gmail.com>
2021-10-07 15:40:29 -0500

    First commit

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import { getGifs } from "../helpers/getGifs";
+
+
+
+export const useFetchGifs = ( category ) =>{
+
+    const [state, setstate] = useState({
+        data:[],
+        loading: true
+    });
+
+    useEffect( () => {
+
+        getGifs( category )
+            .then(imgs =>{
+
+                setTimeout(() =>{
+                    setstate({
+                    data: imgs,
+                    loading: false
+                })
+                },1000);
+
+
+                
+            })
+
+
+    }, [category])
+
+
+
+    return state;
+}
+
